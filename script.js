dcb620172058d87946400529744017911fdeadc5
GaganNahak <160512461+GaganNahak@users.noreply.github.com>
2025-01-27 10:40:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const sharebut=document.getElementById("share-bt")
+const userInput=document.getElementById("inputva").value;
+const mes=`some thing from ${userInput}`
+sharebut.addEventListener("click",()=>{
+    if(navigator.share){
+       navigator.share({
+         title:"gagan",
+        text:mes,
+         url:"https://www.google.com" ,
+       }).then(()=>{
+        console.log("ok")
+       }).catch((err)=>{
+        console.log(err)
+       })
+    }else{
+        alert("unsupport")
+    }
+})
